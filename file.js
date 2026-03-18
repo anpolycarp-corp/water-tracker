6ca80d39d2ba84890ae3c2afa1eb53fbfecd707f
anpolycarp-corp <an.polykarpova@gmail.com>
2026-03-18 13:33:52 +0300

    Init

---
+++ file.js
@@ -0,0 +1,15 @@
+/* Простейший сервис-воркер: офлайн-кеш */
+const CACHE = 'water-tracker-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+});
+self.addEventListener('fetch',e=>{
+  e.respondWith(
+    caches.match(e.request).then(r=>r||fetch(e.request))
+  );
+});
